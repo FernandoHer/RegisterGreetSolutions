4bfe68a20e76485ffe2516b6e9102a88b951c99b
unknown <juanf.herdoia@gmail.com>
2022-05-13 10:08:24 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.services';
+// import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'registro';
+
+export class  AppComponent  {
+
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Juan Perez',[Validators.required, Validators.minLength(6)]],
+    email: ['text5@text.com',[Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+
+  constructor( private fb:FormBuilder,
+              private router:Router,
+              private authServices: AuthService) { }
+
+  register(){
+
+    const {name, email, password} = this.miFormulario.value;
+
+    this.authServices.registro(name, email, password)
+        .subscribe( value => {
+ 
+          if( value === true ){
+            this.router.navigateByUrl('/dashboard')
+          }else {
+              console.log('value', value, 'error')
+            // Swal.fire('E R R O R', value, 'error')
+          }
+        });
+
+    this.router.navigateByUrl('/dashboard')
+  }
+
+ 
+
 }
+
+
+
+
